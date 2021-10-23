3b4b5089ecfa137e2153b4e79851f6501cd58114
Akanksha Mudge <akankshamudage@gmail.com>
2021-10-23 15:00:51 +0530

    code2

---
+++ connect1.php
@@ -0,0 +1,40 @@
+<?php
+
+    $email = filter_input(INPUT_POST, 'email');
+    $password = filter_input(INPUT_POST, 'password');
+
+
+    if(!empty($email)){
+        if(!empty($password)){
+            $host = "localhost:3307";
+            $dbusername = "root";
+            $dbpassword = "";
+            $dbname = "pharmacity";
+
+            //Create a Connection
+            $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+            if(mysqli_connect_error()){
+                die('Connect Error ('.mysqli_connect_errno().')'.mysqli_connect_error());
+            }
+            else{
+                $sql = "INSERT INTO account (email, password) values ('$email', '$password')";
+                if($conn->query($sql)){
+                    header("Location: sign.html");
+                }
+                else{
+                    echo "<h2>Invalid Email or Password</h2>";
+                }
+                $conn->close();
+            }
+        }
+        else{
+            echo "Password should not be empty";
+            die();
+        }
+    }
+    else{
+        echo "Username should not be empty";
+        die();
+    }
+?>
